16091bd836058d7248f881c37ea63e5397ccc757
Kavyan Nayak <kavyannayak104@gmail.com>
2026-03-17 12:12:27 +0530

    C

---
+++ HTML/Backup2/while.c
@@ -0,0 +1,43 @@
+// #include<stdio.h>
+// #include<conio.h>
+
+// void main()
+// {
+//     int i = 0;
+
+//     // clrscr();
+    
+//     // initialize i to 0  -> starting point
+//     i = 5; 
+//     while (i <= 10)
+//     {
+//         printf("%d ", i);
+//         i++; // increment i by 1
+//     }
+    
+//     // getch();
+// }
+
+
+
+// =============do-while loop================
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i;
+
+    // clrscr();
+    
+    // initialize i to 0  -> starting point
+    i = 0; 
+    do
+    {
+        printf("%d ", i);
+        i++; // increment i by 1
+    }while (i <= 10);
+    
+    // getch();
+}
