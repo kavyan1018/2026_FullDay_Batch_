b4632b2fa20e9481c66d0023a275c2e1d6d16e8e
Kavyan Nayak <kavyannayak104@gmail.com>
2026-03-24 12:57:44 +0530

    C

---
+++ C/string/gp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+    char a[20];
+    // clrscr();
+
+    puts("Enter your name : ");
+    gets(a);
+
+
+    puts("Your name is : ");
+    puts(a);
+
+    // getch();
+}
---
+++ C/string/string1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+    char a[20];
+    // clrscr();
+    
+    printf("Enter your name : ");
+    scanf("%s", a);
+
+    printf("Your name is : %s", a);
+
+    // getch();
+}
