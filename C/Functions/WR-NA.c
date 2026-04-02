9128a6732bb3ac7722542969874774f811dcf435
Kavyan Nayak <kavyannayak104@gmail.com>
2026-04-02 10:37:07 +0530

    WR-NA

--- C/Functions/WR-NA.c
+++ C/Functions/WR-NA.c
@@ -1,29 +1,58 @@
+// #include<stdio.h>
+// #include<conio.h>
+// int addfun();
+
+// void main(){
+
+//     int add;
+
+//     add = addfun();
+
+//     printf("\n Addition of 2 numbers : %d", add);
+
+// }
+
+
+// int addfun(){
+
+//     int num1, num2, sum;
+
+//     printf("Enter the First Number :");
+//     scanf("%d", &num1);
+    
+//     printf("Enter the Second Number :");
+//     scanf("%d", &num2);
+
+//     sum = num1 + num2;
+
+//     return sum;
+// }
+
+
+
+
+
+
+
 #include<stdio.h>
 #include<conio.h>
-int addfun();
 
 void main(){
 
-    int add;
+    int a, b;
 
-    add = addfun();
+    int sum;
 
-    printf("\n Addition of 2 numbers : %d", add);
-
-}
-
-
-int addfun(){
-
-    int num1, num2, sum;
+    // clrscr();
 
     printf("Enter the First Number :");
-    scanf("%d", &num1);
+    scanf("%d", &a);    
     
     printf("Enter the Second Number :");
-    scanf("%d", &num2);
+    scanf("%d", &b);
+    sum = a + b;
 
-    sum = num1 + num2;
+    printf("Sum = %d ", sum);
 
-    return sum;
+    // getch();
 }
