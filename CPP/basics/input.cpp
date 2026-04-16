2e978986ec36e4fb1d9ac39df290b83392ef8158
Kavyan Nayak <kavyannayak104@gmail.com>
2026-04-16 09:47:01 +0530

    Basics

---
+++ CPP/basics/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+
+    cout << "Enter a number: ";
+    cin >> a;   // -> cin is used to take input from the user
+
+    cout << "You entered: " << a;
+
+}
---
+++ CPP/basics/intro.cpp
@@ -0,0 +1,23 @@
+// // header files 
+// #include <iostream>
+
+// int main(){
+
+//     // print statement
+
+//     // 1. std -> scope resolution operator -> used to access the cout object from the std namespace
+//     std :: cout << "Hello World!";
+
+// }
+
+
+
+#include <iostream>
+using namespace std;    // -> using directive -> allows us to use names from the std namespace without qualifying them with std::
+
+
+int main(){
+
+    cout << "Hello World!";
+
+}
