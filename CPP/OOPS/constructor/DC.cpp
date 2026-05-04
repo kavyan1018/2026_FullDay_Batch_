d31f7d0f46960234882d1ff52985b74331432fa8
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-04 16:42:03 +0530

    Default Constructor

---
+++ CPP/OOPS/constructor/DC.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class DC{
+
+    public:
+        DC(){
+            cout<<"Default Constructor Called !"<<endl;
+        }
+};
+
+int main(){
+
+    DC obj;    // Default Constructor Called !
+}
