ce3d6f3b83775d62e0280a1ef33da250b32387ab
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-04 17:15:49 +0530

    multi Parameterized Constructor

---
+++ CPP/OOPS/constructor/MPC.cpp
@@ -0,0 +1,65 @@
+// Parameterized Constructor
+
+#include <iostream>
+using namespace std;
+
+class PC
+{
+
+public:
+    string name;
+    int age;
+
+    PC(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    void display()
+    {
+        cout << "Name : " << name << endl;
+        cout << "Age : " << age << endl;
+        cout << "------------------------------" << endl;
+    }
+};
+
+int main()
+{
+
+    int n;
+
+    cout << "Enter Number of People : ";
+    cin >> n;
+
+    string name;
+    int age;
+
+    PC* arr[n];
+
+    /*
+        arr[0] -> points to -> object 1    -> "jone" , 25
+        arr[1] -> points to -> object 2     -> "mike" , 30
+    */
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << "Enter Name : ";
+        cin >> name;
+
+        cout << "Enter Age : ";
+        cin >> age;
+
+        arr[i] = new PC(name, age);    // new -> dynamically memory allocation
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i]->display();   // -> (array of pointers)  -> access object's members
+
+
+                    // . (dot pointer) -> it will access object 
+    }
+
+}
